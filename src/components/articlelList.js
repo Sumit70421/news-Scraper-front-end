6482c70891af79c98556ee1baa0e3dd8e2e79007
Sumit- Patil <69342422+Sumit70421@users.noreply.github.com>
2021-11-13 18:37:39 +0530

    Integrated with apis

--- src/components/articlelList.js
+++ src/components/articlelList.js
@@ -9,22 +9,21 @@ const ArticleList = ({ posts, loading })=>{
   if (loading) {
     return <h2>Loading...</h2>;
   }   
-  var num = 1;
 
       return (
         <>
          <div className="header">
-           <h1>Project Name</h1>
-           <h3>Some Random bullshit about the project i guess?</h3>
+           <h1>Daily News</h1>
+           <h3>Read news headlines on the go!</h3>
          </div>
         
          <ul className='cards'>
            {posts.map(post=> (
-              <li key={num}><Card
-              img={post.imgUrl}
-              title={post.heading}
-              description={post.description}
-              source={post.source} /><span className="hide">{num = num +1}</span></li>
+              <li key={post.index}><Card
+              img={post.news_image}
+              title={post.news_title}
+              description={post.news_content}
+              source={post.news_url} /></li>
            )
            )}
            
